240ac59c28c6c65d5b161a52868abd9ae0085df2
Guilherme Alves <guilherme.santos@eldorado.org.br>
2026-03-30 19:49:41 -0300

    API Secret Config

--- django_min/config/settings.py
+++ django_min/config/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(dotenv_path=BASE_DIR / ".env", override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
