2697556d782c2c48f53f72ed008ffe74e19fb8da
Guilherme Alves <guilherme.santos@eldorado.org.br>
2026-04-28 20:03:44 -0300

    PostgreeSQL Add

--- django_min/config/settings.py
+++ django_min/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,9 +26,9 @@ load_dotenv(dotenv_path=BASE_DIR / ".env", override=True)
 SECRET_KEY = 'django-insecure-8plzj^e3phbev#-x_0*a(huuzrx310t(ai2vw_n)t(h(zzpg%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL = os.environ["DATABASE_URL"]
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 
